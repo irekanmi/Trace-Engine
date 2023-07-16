8d4799fc9d366e97643546c93c9a6fc24e09518e
irekanmi <cokerayanfe@gmail.com>
2023-07-17 00:36:15 +0100

    tweaking

--- assets/shaders/custom.frag.glsl
+++ assets/shaders/custom.frag.glsl
@@ -10,5 +10,5 @@ layout(set = 0, binding = 0)uniform sampler2D color;
 void main()
 {
     vec4 value = texture(color, texCoord);
-    FragColor = vec4((value.x + value.y + value.z + value.w) / 4);
+    FragColor = vec4(vec3((value.x + value.y + value.z) / 3), 1.0f);
 }
